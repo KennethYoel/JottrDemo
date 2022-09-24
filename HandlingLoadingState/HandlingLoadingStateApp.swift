c1291d03300b3437e49efe60c1e3d5de53047207
Ken <kenneth.joel@live.com>
2022-09-24 09:16:09 +0200

    Initial Commit

---
+++ HandlingLoadingState/HandlingLoadingStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandlingLoadingStateApp.swift
+//  HandlingLoadingState
+//
+//  Created by Kenneth Gutierrez on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct HandlingLoadingStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
